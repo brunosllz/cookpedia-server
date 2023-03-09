a5b613f3a31b350af6561cfea99e9a3c2a93e512
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-09 19:41:20 -0300

    refactor(application): changeded initial structure of app, now created a controller and route file

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,5 @@
-import { prisma } from './lib/prisma'
-import { z } from 'zod'
 import { app } from './app'
 import { env } from './env'
-
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { email, name, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      email,
-      name,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
 
 app
   .listen({
