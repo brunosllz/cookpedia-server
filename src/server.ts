9390bfb4421998589969497def07fb5d305b2f50
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-09 19:31:54 -0300

    feat(server): added register a user route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
+import { prisma } from './lib/prisma'
+import { z } from 'zod'
 import { app } from './app'
 import { env } from './env'
 
-app.get('/', (_, reply) => {
-  return reply.send({ message: 'Hello World!' })
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
 
 app
