047b52f06987fcf8af657a1609a02fe590d56cb8
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-11 14:39:48 -0300

    refactor(register): added a instance of prisma user repository and pass to register use case how parameterr

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
 import { z } from 'zod'
+import { PrismaUserRepository } from '@/repositories/prisma-user-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,7 +13,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({ email, name, password })
+    const prismaUserRepository = new PrismaUserRepository()
+
+    const registerUseCase = new RegisterUseCase(prismaUserRepository)
+
+    await registerUseCase.execute({ email, name, password })
   } catch (error) {
     return reply.status(409).send()
   }
