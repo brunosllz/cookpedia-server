d0ea4be5f3e569b1255bf5459d62423f1250feb3
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-11 23:32:29 -0300

    fat(routes): added authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { app } from '@/app'
+import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
 
 export async function appRoutes() {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
