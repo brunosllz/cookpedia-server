ca8e418648d4ef93a1d80768be900eb38c9e0dec
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-12 00:02:44 -0300

    refactor(register): added make register use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { RegisterUseCase } from '@/use-cases/register-use-case'
 import { z } from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,9 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body)
 
   try {
-    const prismaUserRepository = new PrismaUsersRepository()
-
-    const registerUseCase = new RegisterUseCase(prismaUserRepository)
+    const registerUseCase = makeRegisterUseCase()
 
     await registerUseCase.execute({ email, name, password })
   } catch (err) {
