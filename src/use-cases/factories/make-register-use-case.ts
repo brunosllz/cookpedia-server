d30e18e140b9925e3dd7e5c61935ae86d78e8079
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-11 23:58:34 -0300

    feat(useCases): added factories for use cases

---
+++ src/use-cases/factories/make-register-use-case.ts
@@ -0,0 +1,10 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '../register-use-case'
+
+export function makeRegisterUseCase() {
+  const prismaUserRepository = new PrismaUsersRepository()
+
+  const registerUseCase = new RegisterUseCase(prismaUserRepository)
+
+  return registerUseCase
+}
