885d76e1d89b6a80abe6a77705dbdaab69d8097f
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-13 13:55:10 -0300

    refactor(inMemoryRecipesRepository): changeded implementation of repository

--- src/repositories/in-memory/in-memory-recipes-repository.ts
+++ src/repositories/in-memory/in-memory-recipes-repository.ts
@@ -1,38 +1,30 @@
-import { Prisma, Recipe } from '@prisma/client'
-import { randomUUID } from 'crypto'
+import { Recipe } from '@/entities/recipe'
 import { RecipesRepository } from '../recipes-repository'
 
 export class InMemoryRecipesRepository implements RecipesRepository {
-  public database: Recipe[] = []
+  public recipes: Recipe[] = []
 
-  async create(data: Prisma.RecipeUncheckedCreateInput): Promise<Recipe> {
-    const recipe: Recipe = {
-      id: randomUUID(),
-      cookTime: data.cookTime,
-      description: data.description,
-      image_url: data.image_url,
-      origin: data.origin,
-      published_at: data.published_at ? new Date(data.published_at) : null,
-      serve_to: data.serve_to,
-      name: data.name,
-      user_id: data.user_id,
-      created_at: new Date(),
-    }
-
-    this.database.push(recipe)
+  async create(recipe: Recipe) {
+    this.recipes.push(recipe)
 
     return recipe
   }
 
-  async findByNameAndUserId(name: string, userId: string) {
-    const recipe = this.database.find(
-      (recipe) => recipe.name === name && recipe.user_id === userId,
-    )
+  async findById(recipeId: string) {
+    const recipe = this.recipes.find((recipe) => recipe.id === recipeId)
 
     if (!recipe) {
       return null
     }
 
     return recipe
+  }
+
+  async save(recipe: Recipe) {
+    const recipeIndex = this.recipes.findIndex((item) => item.id === recipe.id)
+
+    if (recipeIndex >= 0) {
+      this.recipes[recipeIndex] = recipe
+    }
   }
 }
